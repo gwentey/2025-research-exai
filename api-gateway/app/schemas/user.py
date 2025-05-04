64d5fd2283c3d53a545467e5485d0f5f4a9502ad
Gwentey <anthonyoutub@gmail.com>
2025-05-04 16:16:14 +0200

    fix user register

--- api-gateway/app/schemas/user.py
+++ api-gateway/app/schemas/user.py
@@ -1,7 +1,7 @@
 import uuid
 from fastapi_users import schemas
 from typing import Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 # Modèle Pydantic pour les comptes OAuth
 class OAuthAccountRead(BaseModel):
@@ -26,11 +26,25 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
 # Schéma Pydantic pour la création d'un utilisateur
 class UserCreate(schemas.BaseUserCreate):
     # Inclut par défaut: email, password
+    email: EmailStr  # S'assurer que c'est bien un email
     pseudo: Optional[str] = None
     picture: Optional[str] = None
     given_name: Optional[str] = None
     family_name: Optional[str] = None
     locale: Optional[str] = None
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "user@example.com",
+                "password": "password123",
+                "pseudo": "string",
+                "picture": "string",
+                "given_name": "string",
+                "family_name": "string",
+                "locale": "string"
+            }
+        }
 
 # Schéma Pydantic pour la mise à jour d'un utilisateur (par l'utilisateur lui-même)
 class UserUpdate(schemas.BaseUserUpdate):
