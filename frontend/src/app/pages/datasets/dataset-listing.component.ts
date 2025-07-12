562968a22be0a70067f80be9d955bd5f220cd474
Gwentey <anthonyoutub@gmail.com>
2025-07-12 15:10:52 +0200

    improve design of display datasets

--- frontend/src/app/pages/datasets/dataset-listing.component.ts
+++ frontend/src/app/pages/datasets/dataset-listing.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -36,6 +37,7 @@ interface FilterChip {
     FormsModule,
     ReactiveFormsModule,
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
